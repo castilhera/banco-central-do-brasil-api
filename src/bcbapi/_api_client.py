3a2f91db8a7c70c32e32e3f00dbde924c7f1fca4
Paulo Castilho <32399161+castilhera@users.noreply.github.com>
2024-08-13 17:14:24 -0300

    fix: included request timeout and docstrings

--- src/bcbapi/_api_client.py
+++ src/bcbapi/_api_client.py
@@ -1,31 +1,44 @@
-import requests
-
+from requests import get, post
 
 class _APIClient:
 
     def __init__(self, base_url):
         self.base_url = base_url
-    
+
     def __set_format(self, params:dict[str,str]=None) -> dict[str,str]:
+        """Includes 'format' param to the request.
+        """
         if not params:
             params = {}
         params["format"] = "json"
         return params
-    
-    def __handle_response(self, response: dict):
-        if 'value' in response:
-            return response['value']
+
+    def __parse_response(self, response):
+        """Parse the response.
+        """
+        if response:
+            json = response.json()
+            return json['value'] if 'value' in json else None
+        return None
 
     def get(self, endpoint:str, params:dict[str,str]=None):
+        """Use a HTTP Client to GET data.
+        """
         params = self.__set_format(params)
 
-        response = requests.get(f"{self.base_url}/{endpoint}", params=params)
+        response = get(f"{self.base_url}/{endpoint}",
+                       params=params,
+                       timeout=5)
         response.raise_for_status()
-        return self.__handle_response(response.json())
+        return self.__parse_response(response)
 
     def post(self, endpoint:str, params:dict[str,str]=None):
+        """Use a HTTP Client to POST data.
+        """
         params = self.__set_format(params)
 
-        response = requests.post(f"{self.base_url}/{endpoint}", json=params)
+        response = post(f"{self.base_url}/{endpoint}",
+                        json=params,
+                        timeout=5)
         response.raise_for_status()
-        return self.__handle_response(response.json())
+        return self.__parse_response(response)
