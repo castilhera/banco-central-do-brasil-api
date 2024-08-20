aa7eeef9784b580827fe3f1a5e7a99fc590a11ac
Paulo Castilho <32399161+castilhera@users.noreply.github.com>
2024-08-19 21:27:48 -0300

    fix: updated init to include exchange_rate

--- src/bcbapi/__init__.py
+++ src/bcbapi/__init__.py
@@ -1,12 +1,16 @@
 # from ._api_client import _APIClient
 
-from .models.currencies import Currency
+from .models.currencies import Currency, CurrencyType
 from .services.currency import CurrencyService
 
 from .models.ptax import PTAX
 from .services.ptax import PTAXService
 
+from .models.exchange_rate import ExchangeRate, BulletinType
+from .services.exchange_rate import ExchangeRateService
+
 __all__ = [
-    "CurrencyService", "Currency",
-    "PTAXService", "PTAX"
+    "CurrencyService", "Currency", "CurrencyType",
+    "PTAXService", "PTAX",
+    "ExchangeRateService", "ExchangeRate", "BulletinType"
 ]
