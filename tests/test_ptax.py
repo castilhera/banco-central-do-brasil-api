7452ac1d5b93c81276a006c23d52726acd6b1d53
Paulo Castilho <32399161+castilhera@users.noreply.github.com>
2024-08-19 21:22:24 -0300

    fix: changed to generic var names

--- tests/test_ptax.py
+++ tests/test_ptax.py
@@ -1,30 +1,32 @@
 import unittest
 from datetime import date, datetime
-from bcbapi.services.ptax import PTAX, PTAXService
+from bcbapi import PTAX, PTAXService
 
-class TestPtaxApi(unittest.TestCase):
+class TestPtax(unittest.TestCase):
 
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
-        self.ptax_service = PTAXService()
+        self.service = PTAXService()
 
 
     def test_get_ptax_rate_should_return_none(self):
-        ptax = self.ptax_service.get_ptax_rate(date(2024, 1, 1))
-        self.assertIsNone(ptax)
+        result = self.service.get_ptax_rate(date(2024, 1, 1))
+        self.assertIsNone(result)
 
 
     def test_get_ptax_rate_should_return_ptax(self):
-        ptax = self.ptax_service.get_ptax_rate(date(2024, 1, 2))
-        self.assertEqual(ptax, PTAX(
-            timestamp=datetime(2024, 1, 2, 13, 5, 50, 319000), 
-            buy=4.891, 
+        result = self.service.get_ptax_rate(date(2024, 1, 2))
+        self.assertEqual(result, PTAX(
+            timestamp=datetime(2024, 1, 2, 13, 5, 50, 319000),
+            buy=4.891,
             sell=4.8916
         ))
 
     def test_daily_ptax_rate_by_period_should_return_4(self):
-        daily_ptax_rate_by_period = self.ptax_service.get_daily_ptax_rate_by_period(date(2024, 1, 2), date(2024, 1, 5))
-        self.assertEqual(len(daily_ptax_rate_by_period), 4)
+        result = \
+            self.service.get_daily_ptax_rate_by_period(date(2024, 1, 2),
+                                                       date(2024, 1, 5))
+        self.assertEqual(len(result), 4)
 
 
 if __name__ == '__main__':
